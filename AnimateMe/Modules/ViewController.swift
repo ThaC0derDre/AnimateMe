95baca582ea126ca220dcbd3e596f86ee960bf24
AndresGutierrez <c0dingw.dre@gmail.com>
2021-12-05 20:15:53 -0800

    add json downloaded animations

--- AnimateMe/Modules/ViewController.swift
+++ AnimateMe/Modules/ViewController.swift
@@ -6,9 +6,12 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
