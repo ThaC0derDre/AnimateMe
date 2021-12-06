60c4112c84e7c7ccc8c4e3ef12bec3bd3af3e559
AndresGutierrez <c0dingw.dre@gmail.com>
2021-12-05 18:42:35 -0800

    Initial Commit

---
+++ AnimateMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimateMe
+//
+//  Created by Andres Gutierrez on 12/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
